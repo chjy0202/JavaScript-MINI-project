6ba29a0848cfdb9097814968e0fe2dab99c8dfc7
chjy0202 <chjy0202@naver.com>
2022-06-24 07:51:10 +0900

    [toggle-side-navigation]onoff코드작성. 새로고침 시 저장 안됨

--- toggle side navigation/app.js
+++ toggle side navigation/app.js
@@ -1,1 +1,41 @@
 // do something!
+(function () {
+  const get = (target) => document.querySelector(target);
+
+  const $body = get("body");
+  const $btn = get(".toggle");
+  const $nav = get("nav");
+  let navState = localStorage.getItem("navState");
+
+  const navOnOff = () => {
+    if (navState) {
+      $nav.classList.add("active");
+      localStorage.setItem("navState", true);
+    } else {
+      $nav.classList.remove("active");
+      localStorage.setItem("navState", false);
+    }
+  };
+
+  const navStateCheck = () => {
+    const state = navState;
+    navState = state === null ? false : state;
+
+    navOnOff();
+    $body.style.visibility = "visible";
+  };
+
+  const toggle = () => {
+    navState = !navState;
+    navOnOff();
+    $body.classList.remove("preload");
+  };
+
+  const init = () => {
+    window.addEventListener("DOMContentLoaded", navStateCheck);
+    $btn.addEventListener("click", toggle);
+    // $body.style.visibility = "visible";
+  };
+
+  init();
+})();
