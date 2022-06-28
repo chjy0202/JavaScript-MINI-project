9ea5f115e7496b6a17677105e9e11403956ec69d
chjy0202 <chjy0202@naver.com>
2022-06-28 11:40:12 +0900

    [news-viewer]nav코드 및 API호출

--- news viewer/components/Nav.js
+++ news viewer/components/Nav.js
@@ -1,1 +1,37 @@
 // do something!
+// import { state } from "./state.js";
+
+const Nav = ($container) => {
+  const $categories = {
+    category: {
+      all: "전체보기",
+      business: "비즈니스",
+      entertainment: "엔터테인먼트",
+      health: "건강",
+      science: "과학",
+      sports: "스포츠",
+      technology: "기술",
+    },
+  };
+
+  const $root = $container.querySelector("#root");
+  $root.innerHTML = `
+  <nav class="category-list">
+    <ul>
+      ${Object.entries($categories.category)
+        .map(
+          (item) => `<li id=${item[0]} class="category-item">${item[1]}</li>`
+        )
+        .join("")}
+    </ul>
+  </nav>
+  `;
+
+  // const $nav = $container.querySelector("nav");
+  // $nav.addEventListener("click", (e) => {
+  //   if(e.target.node)
+  // })
+  // document.querySelector(${}).classList.add('active');
+};
+
+export default Nav;
--- news viewer/components/NewsList.js
+++ news viewer/components/NewsList.js
@@ -1,1 +1,52 @@
 // do something!
+import axios from "axios";
+
+let currentPage = 1;
+const limit = 5;
+const apiKey = "efba51b4bc9a4c458b9aa8c0f9e74852";
+
+const NewList = () => {
+  const loadNews = async (category = "all", currentPage) => {
+    const url = `https://newsapi.org/v2/top-headlines?country=kr&category=${
+      category === "all" ? "" : category
+    }&page=${currentPage}&pageSize=${limit}&apiKey=${apiKey}`;
+    try {
+      const { news } = await axios.get(url);
+      return news;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const $root = document.querySelector("#root");
+  $root.innerHTML = `
+	<div class="news-list-container">
+    <article class="news-list">
+      <section class="news-item">
+        <div class="thumbnail">
+          <a href="https://www.ajunews.com/view/20220220180410403" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://image.ajunews.com/content/image/2022/02/20/20220220180523846963.jpg"
+              alt="thumbnail" />
+          </a>
+        </div>
+        <div class="contents">
+          <h2>
+	          <a href="https://www.ajunews.com/view/20220220180410403" target="_blank" rel="noopener noreferrer">
+            	[뉴욕증시 주간전망] 러시아-우크라이나 긴장 속 변동성 지속 - 아주경제
+            </a>
+          </h2>
+          <p>
+            이번 주(21일~25일·현지시간) 뉴욕 증시는 러시아와 우크라이나 간 지정학적 긴장과 우크라이나 간 미국
+            연방준비제도(Fed·연준)의 긴축 우려에 계속해서...
+          </p>
+        </div>
+      </section>
+    </article>
+    <div class="scroll-observer">
+      <img src="img/ball-triangle.svg" alt="Loading..." />
+    </div>
+  </div>
+	`;
+};
+
