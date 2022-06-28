7b8aec74739c988272067434edfad9dfafff223c
chjy0202 <chjy0202@naver.com>
2022-06-28 15:25:59 +0900

    [star-rating]인터렉션 작성

--- star rating/star-rating/index.js
+++ star rating/star-rating/index.js
@@ -1,1 +1,59 @@
 // do something!
+const StarRating = ($container) => {
+  const $links = document.querySelectorAll("link");
+  const newLink = document.createElement("link");
+  newLink.rel = "stylesheet";
+  newLink.href = "star-rating/theme.css";
+  const $lastLink = $links[$links.length - 1];
+  $lastLink.parentNode.insertBefore(newLink, $lastLink.nextSibling);
+
+  const $starRating = document.createElement("div");
+  $starRating.classList = "star-rating-container";
+  $container.appendChild($starRating);
+
+  const limitRating = $container.getAttribute("data-max-rating");
+  for (let i = 0; i < limitRating; i++) {
+    const stars = document.createElement("i");
+    stars.classList.add("bx", "bxs-star");
+    stars.setAttribute("data", `${i + 1}`);
+    $starRating.appendChild(stars);
+  }
+
+  // const $stars = $container.querySelectorAll("i");
+  const $stars = [...$container.querySelectorAll("i")];
+
+  const set = (state) => {
+    for (let i = 0; i < $stars.length; i++) {
+      if ($stars[i].classList.contains(state)) {
+        $stars[i].classList.remove(state);
+      } else return;
+    }
+  };
+  const isShow = (state, e) => {
+    const num = $stars.indexOf(e.target);
+    for (let i = 0; i <= num; i++) {
+      $stars[i].classList.toggle(state);
+    }
+    if (state === "selected") {
+      $container.dispatchEvent(
+        new CustomEvent("rating-change", {
+          detail: num + 1,
+        })
+      );
+    }
+  };
+
+  $starRating.onmouseover = (e) => {
+    set("hovered");
+    isShow("hovered", e);
+  };
+  $starRating.onmouseleave = () => {
+    set("hovered");
+  };
+  $starRating.onclick = (e) => {
+    set("selected");
+    isShow("selected", e);
+  };
+};
+
+export default StarRating;
