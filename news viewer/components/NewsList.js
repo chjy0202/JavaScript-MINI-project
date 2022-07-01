a1a4ea293a4fd139674176273f9d95d98893f011
chjy0202 <chjy0202@naver.com>
2022-07-01 11:13:26 +0900

    [news-viewer]데이터불러오기, 무한스크롤 구현

--- news viewer/components/NewsList.js
+++ news viewer/components/NewsList.js
@@ -1,52 +1,82 @@
 // do something!
-import axios from "axios";
+import { state } from "../state.js";
 
 let currentPage = 1;
 const limit = 5;
-const apiKey = "efba51b4bc9a4c458b9aa8c0f9e74852";
+const apiKey = "520551562b1e4036958e81384ed5ef58";
+// const apiKey = "efba51b4bc9a4c458b9aa8c0f9e74852";
 
-const NewList = () => {
-  const loadNews = async (category = "all", currentPage) => {
+const NewsList = ($container) => {
+  const $newsList = $container.querySelector(".news-list");
+  const $scrollObserver = $container.querySelector(".scroll-observer");
+  // const selectedCategory = null;
+  // const totalNews = 5;
+
+  const creatNewsItem = (article) => {
+    // const { title, description, url, urlToImage } = article
+    // for (const i = 0; i < article.length; i++) {
+    const $article = document.createElement("article");
+    $article.innerHTML = article
+      .map(
+        ({ title, description, url, urlToImage }) => `
+          <section class="news-item">
+            <div class="thumbnail">
+              <a href="${url}" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="${urlToImage}"
+                  alt="thumbnail" />
+              </a>
+            </div>
+            <div class="contents">
+              <h2>
+                <a href="${url}" target="_blank" rel="noopener noreferrer">
+                	${title}
+                </a>
+              </h2>
+              <p>
+                ${description}
+              </p>
+            </div>
+          </section>
+        `
+      )
+      .join("");
+    $newsList.append($article);
+  };
+
+  const loadNews = async (category, currentPage) => {
     const url = `https://newsapi.org/v2/top-headlines?country=kr&category=${
       category === "all" ? "" : category
     }&page=${currentPage}&pageSize=${limit}&apiKey=${apiKey}`;
     try {
-      const { news } = await axios.get(url);
-      return news;
+      const news = await axios.get(url);
+      return news.data.articles;
     } catch (error) {
       console.log(error);
     }
   };
 
-  const $root = document.querySelector("#root");
-  $root.innerHTML = `
-	<div class="news-list-container">
-    <article class="news-list">
-      <section class="news-item">
-        <div class="thumbnail">
-          <a href="https://www.ajunews.com/view/20220220180410403" target="_blank" rel="noopener noreferrer">
-            <img
-              src="https://image.ajunews.com/content/image/2022/02/20/20220220180523846963.jpg"
-              alt="thumbnail" />
-          </a>
-        </div>
-        <div class="contents">
-          <h2>
-	          <a href="https://www.ajunews.com/view/20220220180410403" target="_blank" rel="noopener noreferrer">
-            	[뉴욕증시 주간전망] 러시아-우크라이나 긴장 속 변동성 지속 - 아주경제
-            </a>
-          </h2>
-          <p>
-            이번 주(21일~25일·현지시간) 뉴욕 증시는 러시아와 우크라이나 간 지정학적 긴장과 우크라이나 간 미국
-            연방준비제도(Fed·연준)의 긴축 우려에 계속해서...
-          </p>
-        </div>
-      </section>
-    </article>
-    <div class="scroll-observer">
-      <img src="img/ball-triangle.svg" alt="Loading..." />
-    </div>
-  </div>
-	`;
+  const showNews = async (category, currentPage) => {
+    try {
+      const articles = await loadNews(category, currentPage);
+      creatNewsItem(articles);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const observer = new IntersectionObserver((enteries) => {
+    enteries.forEach(
+      (entry) => {
+        if (entry.isIntersecting) {
+          showNews(state.category, ++currentPage);
+        }
+      },
+      { threshold: 0.5 }
+    );
+  });
+
+  observer.observe($scrollObserver);
 };
 
+export default NewsList;
